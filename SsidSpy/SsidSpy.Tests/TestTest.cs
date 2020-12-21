46f61ce7890583521d2f0dac0166666d66550d52
JingNianNian <JingNianNian@users.noreply.github.com>
2020-12-21 21:59:27 +0800

    增加测试模块

---
+++ SsidSpy/SsidSpy.Tests/TestTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SsidSpy.Tests
+{
+    [TestClass]
+    public class TestTest
+    {
+        [TestMethod]
+        public void ±ØĞëÍ¨¹ı²âÊÔ()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
