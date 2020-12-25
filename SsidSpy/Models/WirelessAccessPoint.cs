bb60a8be9dea9ccb627e4ee199d11489701095d1
MrBBBaiXue <wch272@outlook.com>
2020-12-25 23:45:10 +0800

    Update WirelessAccessPoint Model

---
+++ SsidSpy/Models/WirelessAccessPoint.cs
@@ -0,0 +1,28 @@
+﻿using NativeWifi;
+
+namespace SsidSpy.Models
+{
+    //From https://www.cnblogs.com/ywf520/p/6502452.html
+    class WirelessAccessPoint
+    {
+        public string SSID { get; set; }
+        public string Dot11DefaultAuthAlgorithm { get; set; }
+        public string Dot11DefaultCipherAlgorithm { get; set; }
+        public bool NetworkConnectable { get; set; }
+        public string WlanNotConnectableReason { get; set; }
+        public int WlanSignalQuality { get; set; }
+        public WlanClient.WlanInterface wlanInterface { get; set; }
+
+        public WirelessAccessPoint()
+        {
+            SSID = "NONE";
+            Dot11DefaultAuthAlgorithm = "";
+            Dot11DefaultCipherAlgorithm = "";
+            NetworkConnectable = true;
+            WlanNotConnectableReason = "";
+            WlanSignalQuality = 0;
+            wlanInterface = null;
+        }
+    }
+
+}
