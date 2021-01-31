47e26517b8f6de160d9fc51f5c49fd9de8361acb
MrBBBaiXue <wch272@outlook.com>
2021-01-31 23:28:31 +0800

    初始化Aprheua 解决方案

---
+++ Aprheua/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Aprheua
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new Aprheua.Views.MainWindow();
+            mainWindow.Show();
+        }
+
+    }
+}
