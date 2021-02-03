dd209294f3d5726eaaef014ba608bb96d72e853a
MrBBBaiXue <wch272@outlook.com>
2021-02-03 20:30:41 +0800

    更新MainWindow.xaml *UI未完成，需要PrimaryColor修改和ListView的绘画

--- Aprheua/Views/MainWindow.xaml.cs
+++ Aprheua/Views/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Aprheua.Views
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.GlowWindow
     {
         public MainWindow()
         {
