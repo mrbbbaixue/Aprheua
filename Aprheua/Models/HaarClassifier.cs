a5e0f0818432fb13a832d680d98d67c520a51591
JingNianNian <JingNianNian@users.noreply.github.com>
2021-02-22 10:54:09 +0800

    Create HaarClassifier.cs
    摸啊摸，摸啊摸

---
+++ Aprheua/Models/HaarClassifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace Aprheua.Models
+{
+    public class HaarClassifier
+    {
+        private string _haarClassifierPath { get; }
+        private string _imageInputPath { get; }
+        private string _imageBlockOutputPath { get; }
+        public HaarClassifier(string haarClassifierPath, string imageInputPath, string imageBlockOutputPath)
+        {
+            _haarClassifierPath = haarClassifierPath;
+            _imageInputPath = imageInputPath;
+            _imageBlockOutputPath = imageBlockOutputPath;
+        }
+
+        public List<string> StartHaarClassifier(HaarClassifierChecking haarClassifierChecking)
+        {
+            int count = 1;
+            List<string> OutputImageBlockPath = new List<string>();
+            Mat srcImage = new Mat(_imageInputPath, ImreadModes.AnyColor);
+            CascadeClassifier haarClassifier = new CascadeClassifier(_haarClassifierPath + $@"\{haarClassifierChecking}.xml");
+            Rect[] imageBlockRect = haarClassifier.DetectMultiScale(srcImage, 1.1, 3, 0, new Size(20, 20), new Size(50, 50));
+            foreach (var item in imageBlockRect)
+            {
+                Mat imageBlock = new Mat(srcImage, item);
+                imageBlock.ImWrite(_imageBlockOutputPath + $@"\{haarClassifierChecking}-{count}.jpg");
+                OutputImageBlockPath.Add(_imageBlockOutputPath + $@"\{haarClassifierChecking}-{count}.jpg");
+                count++;
+            }
+            return OutputImageBlockPath;
+        }
+    }
+
+    public enum HaarClassifierChecking
+    {
+        eyesHaarClassifier,
+        noseHaarClassifier,
+        earsHaarClassifier,
+        mouthHaarClassifier
+    }
+}
