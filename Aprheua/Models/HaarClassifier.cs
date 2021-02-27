1e935ddb244b55100402bf9ebbec668a565028fc
JingNianNian <JingNianNian@users.noreply.github.com>
2021-02-27 23:53:45 +0800

    昊哥哥你带带我吧球球了，直说了我感觉我写的就是shit
    a55555555

--- Aprheua/Models/HaarClassifier.cs
+++ Aprheua/Models/HaarClassifier.cs
@@ -19,13 +19,13 @@ namespace Aprheua.Models
             _imageBlockOutputPath = imageBlockOutputPath;
         }
 
-        public List<string> StartHaarClassifier(HaarClassifierChecking haarClassifierChecking)
+        public List<string> StartHaarClassifier(HaarClassifierChecking haarClassifierChecking, int nDetection, int minSize, int maxSize)
         {
             int count = 1;
             List<string> OutputImageBlockPath = new List<string>();
             Mat srcImage = new Mat(_imageInputPath, ImreadModes.AnyColor);
             CascadeClassifier haarClassifier = new CascadeClassifier(_haarClassifierPath + $@"\{haarClassifierChecking}.xml");
-            Rect[] imageBlockRect = haarClassifier.DetectMultiScale(srcImage, 1.1, 3, 0, new Size(20, 20), new Size(50, 50));
+            Rect[] imageBlockRect = haarClassifier.DetectMultiScale(srcImage, 1.1, nDetection, 0, new Size(minSize, minSize), new Size(maxSize, maxSize));
             foreach (var item in imageBlockRect)
             {
                 Mat imageBlock = new Mat(srcImage, item);
