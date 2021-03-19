2a89fdab2825c9f247d6cf209f0a0287b2785257
MrBBBaiXue <wch272@outlook.com>
2021-03-19 18:08:56 +0800

    1.0 Release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Aprheua")]
-[assembly: AssemblyDescription("Opera Image Split And Analyse Program")]
+[assembly: AssemblyDescription("Aprheua HAAR Classifier GUI Program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("?")]
 [assembly: AssemblyProduct("Aprheua")]
@@ -49,5 +49,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.4.0")]
-[assembly: AssemblyFileVersion("2.5.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
